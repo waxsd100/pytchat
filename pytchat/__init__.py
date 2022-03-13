82bf03c1bdacefd543be7031223cf0f227eeadce
Aiagate <50893541+Aiagate@users.noreply.github.com>
2022-03-13 12:08:33 +0900

    Change version.

--- pytchat/__init__.py
+++ pytchat/__init__.py
@@ -2,10 +2,10 @@
 pytchat is a lightweight python library to browse youtube livechat without Selenium or BeautifulSoup.
 """
 __copyright__    = 'Copyright (C) 2019, 2020, 2021 taizan-hokuto'
-__version__      = '0.5.5'
+__version__      = '0.5.6'
 __license__      = 'MIT'
-__author__       = 'taizan-hokuto'
-__author_email__ = '55448286+taizan-hokuto@users.noreply.github.com'
+__author__       = 'Aiagate'
+__author_email__ = '50893541+Aiagate@users.noreply.github.com'
 __url__          = 'https://github.com/taizan-hokuto/pytchat'
 
 
