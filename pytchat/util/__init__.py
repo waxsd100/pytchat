c6e1dcdda90c7eb974e56cdf763dd3e22fb7a33c
JohnSmith <35010382+1jtp8sobiu@users.noreply.github.com>
2022-08-23 19:19:12 +0900

    Update __init__.py

--- pytchat/util/__init__.py
+++ pytchat/util/__init__.py
@@ -125,7 +125,7 @@ def get_channelid_2nd(client, video_id):
 
 
 async def get_channelid_async(client, video_id):
-    resp = await client.get("https://www.youtube.com/embed/{}".format(quote(video_id)), headers=config.headers)  
+    resp = await client.get("https://www.youtube.com/watch?v={}".format(quote(video_id)), headers=config.headers)  
     match = re.search(PATTERN_CHANNEL, resp.text)
     try:
         if match is None:
